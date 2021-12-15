78933d6021d83ad1be9f9441741b6fdfa76ba44c
jologzxcc <lourdzitro@gmail.com>
2021-12-15 09:58:52 +0800

    Test Changes

--- Firmware/hand_estimator.py
+++ Firmware/hand_estimator.py
@@ -11,6 +11,7 @@ mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 class_and_coordinates = ""
 
+
 def estimate_hands():
   with mp_hands.Hands(
       model_complexity = 0,
@@ -37,13 +38,13 @@ def estimate_hands():
         
         if results.multi_hand_landmarks != None:
           
-          # for hand_landmarks in results.multi_hand_landmarks:
-          #   mp_drawing.draw_landmarks(
-          #       image,
-          #       hand_landmarks,
-          #       mp_hands.HAND_CONNECTIONS,
-          #       mp_drawing_styles.get_default_hand_landmarks_style(),
-          #       mp_drawing_styles.get_default_hand_connections_style())
+          for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
 
           for handLandmarks in results.multi_hand_landmarks:
             
