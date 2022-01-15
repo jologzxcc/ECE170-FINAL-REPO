63ec070650f65ed4ad0987036ce3e172006ff631
jologzxcc <lourdzitro@gmail.com>
2022-01-15 15:16:08 +0800

    Adding Serial communication to the script

--- Firmware/hand_estimator.py
+++ Firmware/hand_estimator.py
@@ -1,8 +1,11 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import serial
+import time
 
-from inverse_kinematics import inv
+from inverse_kinematics import inv, scaled_angles
+from arduino import send_to_arduino
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -11,6 +14,8 @@ mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 class_and_coordinates = ""
 
+arduino = serial.Serial(port='COM3', baudrate=9600)
+arduino.timeout = 1
 
 def estimate_hands():
   with mp_hands.Hands(
@@ -34,7 +39,6 @@ def estimate_hands():
 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
         
         if results.multi_hand_landmarks != None:
           
@@ -68,7 +72,6 @@ def estimate_hands():
             # normalized_landmark_5 = handLandmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP.value]
             # pixel_coordinates_landmark_5 = mp_drawing._normalized_to_pixel_coordinates(normalized_landmark_5.x, normalized_landmark_5.y, img_width, img_height)
             
-        
             # cx = int((pixel_coordinates_landmark_wrist[0] + pixel_coordinates_landmark_5[0])/2)
             # cy = int((pixel_coordinates_landmark_wrist[1] + pixel_coordinates_landmark_5[1])/2)
 
@@ -78,15 +81,16 @@ def estimate_hands():
           
             inverse = inv((dot))
             joints = inverse[1]
+            new_angles = scaled_angles(inverse[0])
 
             image = cv2.line(image, (int(joints[0].x), int(joints[0].y)), (int(joints[1].x), int(joints[1].y)), color=(0, 255, 0), thickness=2)
             image = cv2.line(image, (int(joints[1].x), int(joints[1].y)), (int(joints[2].x), int(joints[2].y)), color=(0, 255, 0), thickness=2)
             image = cv2.line(image, (int(joints[2].x), int(joints[2].y)), (int(joints[3].x), int(joints[3].y)), color=(0, 255, 0), thickness=2)
-
             image = cv2.circle(image, dot[:2], radius=10, color=(0, 0, 255), thickness=-1)
-
             
-            print(class_and_coordinates + f", coordinates {dot}" + f", angles: {inverse[0]}")
+            print(class_and_coordinates + f", coordinates {dot}" + f", angles: {inverse[0]}" + f", new angles: {new_angles}")
+
+            arduino.write(new_angles.encode())
 
         cv2.imshow('Detected Hands', cv2.flip(image, 1))
 
--- Firmware/inverse_kinematics.py
+++ Firmware/inverse_kinematics.py
@@ -3,12 +3,25 @@ from vectormath import Vector2
 initial_joint_positions = [Vector2(0, 0), Vector2(350, 265), Vector2(550, 415), Vector2(650, 490)]
 tolerance = 0.02
 
-# Initialize the Fabrik class (Fabrik, Fabrik2D or Fabrik3D)
 fab = pyfabrik.Fabrik2D(initial_joint_positions, tolerance)
 
 def inv(coor):
     fab.move_to(Vector2(coor))
-    x = fab.angles_deg # Holds [0.0, 0.0, 0.0]
+    x = fab.angles_deg 
     return x, fab.joints
+
+def scaled_angles(orig_angles):
+    new_angles = []
+    for angle in orig_angles:
+        if angle >= 0: 
+            new_angles.append(angle + 90)
+        else: 
+            new_angles.append(angle + 90)
+    
+    str_angles = ' '.join([str(elem) for elem in new_angles])
+    
+    return str_angles
+
+
 
 
